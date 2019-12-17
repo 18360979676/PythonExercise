47740ab159b593776cab06579156587b001d7588
yezhu <18360979676@163.com>
2019-12-18 03:02:13 +0800

    更新操作

--- spider/download_txts.py
+++ spider/download_txts.py
@@ -4,7 +4,7 @@ __author__ = 'Administrator'
 
 from bs4 import BeautifulSoup
 
-import requests, sys
+import requests, sys, string, random
 
 """
 类说明:下载《笔趣看》网小说《一念永恒》
@@ -17,7 +17,7 @@ class DownLoader(object):
     """
     def __init__(self):
         self.server = 'http://www.biqukan.com/'
-        self.target = 'http://www.biqukan.com/1_1094/'
+        self.target = 'http://www.biqukan.com/42_42882/'
         self.names = []  # 存放章节名
         self.urls = []   # 存放章节链接
         self.nums = []   # 章节数
@@ -25,9 +25,18 @@ class DownLoader(object):
     函数说明：获取下载链接
     """
     def get_download_url(self):
-        req = requests.get(url=self.target)
+        header_list = [
+            # 遨游
+            {"user-agent": "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)"},
+            # 火狐
+            {"user-agent": "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1"},
+            # 谷歌
+            {
+                "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11"}
+        ]
+        req = requests.get(url=self.target, headers=random.choice(header_list))
         req.encoding = 'gbk'  # 指定网页编码类型
-        html = req.text()
+        html = req.content
         div_bf = BeautifulSoup(html, "html5lib")
         div = div_bf.find_all('div', class_='listmain')
         a_bf = BeautifulSoup(str(div[0]), "html5lib")
@@ -64,7 +73,7 @@ if __name__ == '__main__':
     dl.get_download_url()
     print('《一念永恒》开始下载：')
     for i in range(dl.nums):
-        dl.writer(dl.names[i], '../publicFile/一念永恒.txt', dl.get_contents(dl.urls[i]))
+        dl.writer(dl.names[i], '../publicFile/叶凡唐若雪.txt', dl.get_contents(dl.urls[i]))
         sys.stdout.write("已下载:%.3f%%" % float(i/dl.nums) + '\r')
         sys.stdout.flush()
-    print('《一念永恒》下载完成')
+    print('《叶凡唐若雪》下载完成')
